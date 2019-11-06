6655e6bfc56440fff79e043f40dd02402b1a4d6b
Mateusz Laskowski <mati1003.ml@gmail.com>
2019-11-06 18:06:01 +0100

    Ukończone dodawanie i formatowanie. Pozostała walidacja.

--- Pocket Trainer/platforms/browser/www/js/controllers/nazwaCwiczeniaCtrl.js
+++ Pocket Trainer/platforms/browser/www/js/controllers/nazwaCwiczeniaCtrl.js
@@ -4,6 +4,7 @@ angular.module('app.controllers')
 function ($scope, $stateParams, Library) {
 
     const muscleParts = Library.getMusclePart()
+    $scope.distance = ""
 
     $scope.$on('$ionicView.beforeEnter', () => {       
         $scope.infoExe = $stateParams.param
@@ -17,7 +18,23 @@ function ($scope, $stateParams, Library) {
         else {
             let minutes =  Math.floor(time/60)
             let seconds = time-(minutes*60)
+            if(seconds === 0) {
+                return `${minutes}min`
+            }
             return `${minutes}min ${pad(seconds)}s`
+        }
+    }
+
+    $scope.convertDistance = (distance) => {
+        if($scope.infoExe.value_type === 'DISTANCE') {
+            console.log(distance)
+            let kilometers = Math.floor(distance/1000)
+            console.log("KM: ", kilometers)
+            let meters = distance - (kilometers*1000)
+            console.log("M: ", meters)
+            if(kilometers === 0) { return `${meters}m` }
+            else if(meters === 0) { return `${kilometers} km` }
+            else { return `${kilometers}km ${meters}m` }
         }
     }
 
--- Pocket Trainer/platforms/browser/www/js/controllers/noweCwiczenieCtrl.js
+++ Pocket Trainer/platforms/browser/www/js/controllers/noweCwiczenieCtrl.js
@@ -18,9 +18,36 @@ function ($scope, $stateParams, Library) {
         const userExe = JSON.parse(window.localStorage.getItem('userExe'))
         if(userExe != null) { listOfUserExe = userExe.value }
         let defaultVal = null
-        if(exe.reps > 0) { defaultVal = exe.reps }
-        else if(exe.distance > 0) { defaultVal = exe.distance }
-        else if(exe.time > 0) { defaultVal = exe.time }
+        // TIME
+        if(exe.selectedType.id == 'TIME') {
+            let minutes = 0
+            let seconds = 0
+            if(exe.time.minutes != null && exe.time.minutes > 0) {
+                minutes = parseInt(exe.time.minutes)
+            }
+            if(exe.time.seconds != null && exe.time.seconds > 0) {
+                seconds = parseInt(exe.time.seconds)
+            }
+            defaultVal = (minutes*60) + seconds
+        }
+        // DISTANCE
+        if(exe.selectedType.id == 'DISTANCE') {
+            let kilometers = 0
+            let meters = 0
+            if(exe.dist.kilometers != null && exe.dist.kilometers > 0) {
+                kilometers = exe.dist.kilometers
+            }
+            if(exe.dist.meters != null && exe.dist.meters > 0) {
+                meters = exe.dist.meters
+            }
+            defaultVal = (kilometers*1000) + meters
+        }
+        // REPS
+        if(exe.selectedType.id == 'REPS') { defaultVal = exe.reps }
+
+        // if(exe.reps > 0) { defaultVal = exe.reps }
+        // else if(exe.distance > 0) { defaultVal = exe.distance }
+        // else if(exe.time > 0) { defaultVal = exe.time }
         let jsonObject = {
             "name": exe.name,
             "exe_type": exe.selectedType.id,
