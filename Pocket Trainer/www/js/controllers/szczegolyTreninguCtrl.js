225e0605e875c111d4591be9ba8e81f9ee9db95a
piotryks <piotryks@gmail.com>
2019-11-16 00:20:54 +0100

    Wyświetlanie szczegółów treningu

--- Pocket Trainer/www/js/controllers/szczegolyTreninguCtrl.js
+++ Pocket Trainer/www/js/controllers/szczegolyTreninguCtrl.js
@@ -3,8 +3,11 @@ angular.module('app.controllers')
     .controller('szczegolyTreninguCtrl', ['$scope', '$stateParams',
         function ($scope, $stateParams) {
 
+            $scope.training = {}
+
             $scope.$on('$ionicView.beforeEnter', () => {   
-                console.log('Trening: ',$stateParams.training)
+                $scope.training = $stateParams.training
+                console.log('Trening: ',$scope.training)
             });
 
         }])
