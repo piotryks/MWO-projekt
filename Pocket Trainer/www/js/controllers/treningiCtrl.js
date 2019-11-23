7a21cf380ba42d34fe9e1707f061f44c3196a1a2
piotryks <piotryks@gmail.com>
2019-11-24 00:53:58 +0100

    Usuwanie treningów

--- Pocket Trainer/www/js/controllers/treningiCtrl.js
+++ Pocket Trainer/www/js/controllers/treningiCtrl.js
@@ -1,9 +1,22 @@
 angular.module('app.controllers')
 
-    .controller('treningiCtrl', ['$scope', '$stateParams', 'CreateTraining', '$state', 'ApiRequest',
-        function ($scope, $stateParams, CreateTraining, $state, ApiRequest) {
+    .controller('treningiCtrl', ['$scope', '$stateParams', 'CreateTraining', '$state', 'ApiRequest','$ionicPlatform',
+        function ($scope, $stateParams, CreateTraining, $state, ApiRequest,$ionicPlatform) {
 
             $scope.reqRes = null
+            $scope.trainToRemove = null
+            $scope.removePopup = false
+
+            // przechwytywanie back buttona
+            $ionicPlatform.registerBackButtonAction(function (e) {
+                if ($scope.removePopup) {
+                    $scope.removePopup = false
+                    e.preventDefault()
+                    $scope.$apply()
+                } else {
+                    navigator.app.backHistory()
+                }
+              }, 100);
 
             $scope.$on('$ionicView.beforeEnter', () => {   
                 $scope.userTrainings = JSON.parse(window.localStorage.getItem('userTraining'))
@@ -31,6 +44,26 @@ angular.module('app.controllers')
                 }).catch( err => {
                     $scope.reqRes = false
                 })
+            }
+
+            $scope.deleteTraining = (training) => {
+                console.log("Trenieng do usunięcia: ", training)
+                $scope.trainToRemove = training
+                $scope.removePopup = true
+            }
+
+            $scope.deleteDismiss = () => {
+                $scope.removePopup = false
+                $scope.trainToRemove = null
+            }
+
+            $scope.deleteConfirm = () => {
+                $scope.userTrainings.splice( $scope.userTrainings.indexOf($scope.trainToRemove), 1 )
+                window.localStorage.removeItem('userTraining')
+                window.localStorage.setItem('userTraining', JSON.stringify($scope.userTrainings))
+                $scope.removePopup = false
+                $scope.trainToRemove = null
+                $scope.$apply()
             }
 
         }])
